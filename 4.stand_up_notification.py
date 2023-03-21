17124bfba1cacae38e824b8abe9914d16723e535
ewenmunro <87114580+ewenmunro@users.noreply.github.com>
2023-03-21 15:28:21 +1100

    Update 4.stand_up_notification.py

--- 4.stand_up_notification.py
+++ 4.stand_up_notification.py
@@ -11,30 +11,33 @@ last_alert_time = 0
 while True:
     # Check if the computer is awake
     is_awake = os.popen('ioreg -c IODisplayWrangler | grep -i IOPowerManagement').read()
-    is_awake = 'PowerManagementAssertion' in is_awake
-    
-    # If the computer is awake, increment the timer
+    is_awake = 'CurrentPowerState"=4' in is_awake
+
     if is_awake:
         current_time = time.time()
         elapsed_time = current_time - last_alert_time
-        
-        # Check if an hour has passed since the last alert
+
         if elapsed_time >= 3600:
             # Send a notification
             logging.info('Sending notification to stand up and move around')
             os.system("""
-                      osascript -e 'display notification "Get off your butt!" sound name "default" '
+                      osascript -e 'display notification "Get off your butt!" with title "Stand Up Notification"'
                       """)
             os.system('afplay /System/Library/Sounds/Glass.aiff')
             print('Sent notification at', time.strftime('%Y-%m-%d %H:%M:%S'))
-            
+
             # Save the current time as the last alert time
             last_alert_time = current_time
             print('Updated last alert time to', time.strftime('%Y-%m-%d %H:%M:%S'))
+
+        # Wait for the remaining time in the hour before checking again
+        remaining_time = (3600 - elapsed_time) % 3600
+        time.sleep(remaining_time)
+        print('Checked at', time.strftime('%Y-%m-%d %H:%M:%S'))
     else:
         # Reset the timer if the computer is asleep
         last_alert_time = 0
-    
-    # Wait for a minute before checking again
-    time.sleep(60)
-    print('Checked at', time.strftime('%Y-%m-%d %H:%M:%S'))
+
+        # Wait for a minute before checking again
+        time.sleep(60)
+        print('Checked at', time.strftime('%Y-%m-%d %H:%M:%S'))
