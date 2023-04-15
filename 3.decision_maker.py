f3344273356716b1f85aa6a9994124b99d8c1a8f
ewenmunro <87114580+ewenmunro@users.noreply.github.com>
2023-04-15 11:02:59 +1000

    Update 3.decision_maker.py

--- 3.decision_maker.py
+++ 3.decision_maker.py
@@ -1,7 +1,21 @@
 from random import choice
 
 # ask user to input options
-options = input("Enter a list of options, separated by commas: ").split(",")
+options_str = input("Enter a list of options, separated by commas: ")
+
+# replace any additional whitespace characters with a single space
+options_str = " ".join(options_str.split())
+
+# split options by comma
+options = options_str.split(",")
+
+# capitalize options that begin with a word
+capitalized_options = []
+for option in options:
+    if option[0].isalpha():
+        capitalized_options.append(option.strip().capitalize())
+    else:
+        capitalized_options.append(option.strip())
 
 # print result
-print(choice(options))
+print(f"{choice(capitalized_options)}")
